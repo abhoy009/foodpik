262e24aece7a64d23ada42a77a366b587b03c0e0
abhoy009 <abhoy009@gmail.com>
2024-09-28 21:39:30 +0530

    Add remaining project files

---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+const CDN_URL = 
+"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+
+const LOGO_URL = 
+"https://d1csarkz8obe9u.cloudfront.net/posterpreviews/food-delivery-logo-design-template-a342a3b7515a10c800c6885bce8b3ae9_screen.jpg?ts=1619678752";
